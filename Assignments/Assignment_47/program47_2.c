76a4035da43c77fce9688e2e9913c6b6db8814ef
TejasSutar9 <tejassutar9001@gmail.com>
2026-01-04 08:17:04 +0530

    Update program47_2.c

--- Assignments/Assignment_47/program47_2.c
+++ Assignments/Assignment_47/program47_2.c
@@ -14,12 +14,16 @@ typedef struct node **PPNODE;
 // Insert node at beginning
 void InsertFirst(PPNODE first, int no)
 {
-    PNODE newn = (PNODE)malloc(sizeof(NODE));
+    PNODE newn = NULL;
+    
+    newn = (PNODE)malloc(sizeof(NODE));
     newn->data = no;
     newn->next = NULL;
 
     if (*first == NULL)
+    {
         *first = newn;
+    }
     else
     {
         newn->next = *first;
@@ -104,3 +108,4 @@ int main()
 
     return 0;
 }
+
